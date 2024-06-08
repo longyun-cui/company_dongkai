d0832b8b38874b60ed89e17be96c927761a7d7b8
Lonyun Cui <longyun-cui@163.com>
2024-06-09 01:33:38 +0800

    detail

--- resources/views/dk/admin/entrance/statistic/statistic-staff-customer-service.blade.php
+++ resources/views/dk/admin/entrance/statistic/statistic-staff-customer-service.blade.php
@@ -347,6 +347,40 @@
                             }
                         },
                         {
+                            "title": "重复",
+                            "data": "order_count_for_delivered_repeated",
+                            "className": "bg-delivered",
+                            "width": "80px",
+                            "orderable": true,
+                            "orderSequence": ["desc", "asc"],
+                            "fnCreatedCell": function (nTd, data, row, iRow, iCol) {
+                                if(row.id == "统计")
+                                {
+                                    $(nTd).addClass('_bold');
+                                }
+                            },
+                            render: function(data, type, row, meta) {
+                                return data;
+                            }
+                        },
+                        {
+                            "title": "拒绝",
+                            "data": "order_count_for_delivered_rejected",
+                            "className": "bg-delivered",
+                            "width": "80px",
+                            "orderable": true,
+                            "orderSequence": ["desc", "asc"],
+                            "fnCreatedCell": function (nTd, data, row, iRow, iCol) {
+                                if(row.id == "统计")
+                                {
+                                    $(nTd).addClass('_bold');
+                                }
+                            },
+                            render: function(data, type, row, meta) {
+                                return data;
+                            }
+                        },
+                        {
                             "title": "有效交付率",
                             "data": "order_rate_for_delivered_effective",
                             "className": "bg-delivered",
