32dffc4d8dc990d065b5adb46245585f669cffc8
Lonyun Cui <longyun-cui@163.com>
2024-06-08 21:12:27 +0800

    detail

--- resources/views/dk/admin/layout/layout-style.blade.php
+++ resources/views/dk/admin/layout/layout-style.blade.php
@@ -1,4 +1,17 @@
 <style>
     .select2-container { height:100%; border-radius:0; float:left; }
     .select2-container .select2-selection--single { border-radius:0; }
+    .main-sidebar, .left-side {
+        width: 120px;
+    }
+    .content-wrapper, .right-side, .main-footer {
+        margin-left: 120px;
+    }
+    .sidebar-menu>li>a {
+        padding: 12px 4px 12px 8px;
+        font-size:12px;
+    }
+    .sidebar-menu>li>a span {
+        font-size:12px;
+    }
 </style>
