5c5f2183bf3cd134f97b83fd48a25a661c3ace5d
Lonyun Cui <longyun-cui@163.com>
2024-11-11 13:24:04 +0800

    choice

--- resources/views/dk/admin_2/layout/main-header.blade.php
+++ resources/views/dk/admin_2/layout/main-header.blade.php
@@ -6,7 +6,7 @@
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>A</b></span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>录单系统</b></span>
+        <span class="logo-lg"><b>自选系统</b></span>
     </a>
 
     <!-- Header Navbar -->
