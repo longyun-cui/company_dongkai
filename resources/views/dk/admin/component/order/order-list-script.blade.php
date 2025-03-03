218f832f3dea6b20575691b8c11214ab8945040d
Lonyun Cui <longyun-cui@163.com>
2025-03-04 07:42:56 +0800

    admin

--- resources/views/dk/admin/component/order/order-list-script.blade.php
+++ resources/views/dk/admin/component/order/order-list-script.blade.php
@@ -1536,6 +1536,7 @@
 
 
 
+        // 【交付列表】【批量操作】批量-导出
         $(".main-content").on('click', '#bulk-submit-for-delivery-export', function() {
             // var $checked = [];
             // $('input[name="bulk-id"]:checked').each(function() {
@@ -1552,6 +1553,71 @@
 
             var $url = url_build('/statistic/statistic-export-for-order-by-ids?ids='+$ids);
             window.open($url);
+
+        });
+        // 【交付列表】【批量操作】批量-更改导出状态
+        $(".main-content").on('click', '#bulk-submit-for-exported', function() {
+            // var $checked = [];
+            // $('input[name="bulk-id"]:checked').each(function() {
+            //     $checked.push($(this).val());
+            // });
+            // console.log($checked);
+
+            var $ids = '';
+            $('input[name="bulk-id"]:checked').each(function() {
+                $ids += $(this).val()+'-';
+            });
+            $ids = $ids.slice(0, -1);
+
+
+            layer.msg('确定"批量导出"么', {
+                time: 0
+                ,btn: ['确定', '取消']
+                ,yes: function(index){
+
+                    $.post(
+                        "{{ url('/item/delivery-bulk-exported') }}",
+                        {
+                            _token: $('meta[name="_token"]').attr('content'),
+                            operate: "delivery-exported-bulk",
+                            ids: $ids,
+                            operate_result:$('select[name="bulk-operate-status"]').val()
+                        },
+                        function(data){
+                            layer.close(index);
+                            if(!data.success) layer.msg(data.msg);
+                            else
+                            {
+                                // $('#datatable_ajax').DataTable().ajax.reload(null,false);
+
+                                $('input[name="bulk-id"]:checked').each(function() {
+
+                                    var $that = $(this);
+                                    var $row = $that.parents('tr');
+
+                                    var $operate_result = $('select[name="bulk-operate-status"]').val();
+
+                                    if($operate_result == "1")
+                                    {
+                                        $row.find('td[data-key=is_exported]').html('<small class="btn-xs btn-success">已导出</small>');
+                                    }
+                                    else if($operate_result == "0")
+                                    {
+                                        $row.find('td[data-key=is_exported]').html('<small class="btn-xs btn-primary">未导出</small>');
+                                    }
+                                    else
+                                    {
+                                    }
+
+
+                                });
+                            }
+                        },
+                        'json'
+                    );
+
+                }
+            });
 
         });
 
