ca794c8a78d29f2ec24811ee544c2e4dbfd9b815
Lonyun Cui <longyun-cui@163.com>
2025-05-10 00:35:04 +0800

    admin

--- app/Repositories/DK/DKAdminRepository.php
+++ app/Repositories/DK/DKAdminRepository.php
@@ -8832,8 +8832,8 @@ class DKAdminRepository {
             // 单均通话 & 单均分钟
             if($v['order_count_for_all'] > 0)
             {
-                $merged[$k]['cnt_per'] = round(($v['cnt'] / $v['order_count_for_all']),2);
-                $merged[$k]['minutes_per'] = round(($v['minutes'] / $v['order_count_for_all']),2);
+                if(!empty($v['cnt'])) $merged[$k]['cnt_per'] = round(($v['cnt'] / $v['order_count_for_all']),2);
+                if(!empty($v['minutes']))  $merged[$k]['minutes_per'] = round(($v['minutes'] / $v['order_count_for_all']),2);
             }
             else
             {
