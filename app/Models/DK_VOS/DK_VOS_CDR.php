c2387e4151342af53d734f453df257007b22cdf2
Lonyun Cui <longyun-cui@163.com>
2025-06-06 23:16:34 +0800

    admin

--- app/Models/DK_VOS/DK_VOS_CDR.php
+++ app/Models/DK_VOS/DK_VOS_CDR.php
@@ -13,7 +13,7 @@ class DK_VOS_CDR extends Authenticatable
 //    protected $connection = 'mysql0';
 //    protected $connection = 'mysql_def';
 
-    protected $table = "vos_e_cdr";
+    protected $table = "a_vos_e_cdr";
 //    protected $table = "dk_vos";
 
     protected $fillable = [
