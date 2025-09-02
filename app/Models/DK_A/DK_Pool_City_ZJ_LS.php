8b1abbea247a3322e2747d30a8dc6e63544fad79
Lonyun Cui <longyun-cui@163.com>
2025-09-02 12:29:32 +0800

    cc

--- app/Models/DK_A/DK_Pool_City_ZJ_LS.php
+++ app/Models/DK_A/DK_Pool_City_ZJ_LS.php
@@ -5,11 +5,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 use DB;
 
-class DK_Pool_City_HeN_SC extends Model
+class DK_Pool_City_ZJ_LS extends Model
 {
 //    use SoftDeletes;
     //
-    protected $table = "a_pool_city_hen_sc";
+    protected $table = "a_pool_city_zj_ls";
     protected $fillable = [
         'active', 'status', 'category', 'type', 'form', 'sort',
         'item_active', 'item_status', 'item_result', 'item_category', 'item_type', 'item_form',
