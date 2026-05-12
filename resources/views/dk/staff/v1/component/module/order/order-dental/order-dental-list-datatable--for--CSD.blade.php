48616ab778c433bd5c712a59e4fb8fbc5ef26a19
longyun-cui <longyun.cui88@gmail.com>
2026-05-12 15:57:12 +0800

    admin

--- resources/views/dk/staff/v1/component/module/order/order-dental/order-dental-list-datatable--for--CSD.blade.php
+++ resources/views/dk/staff/v1/component/module/order/order-dental/order-dental-list-datatable--for--CSD.blade.php
@@ -228,7 +228,7 @@
                         if(data == "通过")
                         {
                             $result_html = '<small class="btn-xs bg-green">'+data+'</small>';
-                            if($inspected_result_2)
+                            if($inspected_result_2 && ['一档','二档','三挡'].includes(row.inspected_result))
                             {
                                 $result_2_html = '<small class="btn-xs bg-green">'+$inspected_result_2+'</small>';
                             }
