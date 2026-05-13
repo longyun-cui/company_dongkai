8d7b14c8f3b525c4c924878f51af34a6c3f5fc92
longyun-cui <longyun.cui88@gmail.com>
2026-05-13 09:32:03 +0800

    admin

--- app/Jobs/DK/DK_AI_Inspect_Job.php
+++ app/Jobs/DK/DK_AI_Inspect_Job.php
@@ -307,7 +307,6 @@ class DK_AI_Inspect_Job implements ShouldQueue
                             $content_fix_2 = robustJsonFixer($content_fix);
                             $content_decode = json_decode($content_fix_2);
                         }
-                        else $content_decode = null;
                     }
 
 
