efa9e65a767284464715e0e048184e84a04041ea
longyun-cui <longyun.cui88@gmail.com>
2026-06-08 01:25:09 +0800

    admin

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
@@ -23,4 +25,19 @@ class VerifyCsrfToken extends Middleware
         'cc_api/okcc/*',
         'choice_api/okcc/*'
     ];
+
+
+    public function handle($request, Closure $next)
+    {
+        // api.xx.com 完全不走 CSRF（无状态 API）
+//        if ($request->getHost() === env('DOMAIN_DK_API'))
+        if ($request->getSchemeAndHttpHost() === env('DOMAIN_DK_API'))
+        {
+            return $next($request);
+        }
+
+        return parent::handle($request, $next);
+    }
+
+
 }
