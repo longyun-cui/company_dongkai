eb6f0186d63b9838ca8b704f5c9a76e6e5e7d762
longyun-cui <longyun.cui88@gmail.com>
2026-08-10 10:49:32 +0800

    admin

--- resources/views/dk/staff/v1/component/module/order/order-dental/order-dental-list-datatable--for--AD.blade.php
+++ resources/views/dk/staff/v1/component/module/order/order-dental/order-dental-list-datatable--for--AD.blade.php
@@ -228,6 +228,7 @@
                     render: function(data, type, row, meta) {
                         if(!row.inspected_at) return '--';
                         var $result_html = '';
+                        var $result_2_html = '';
                         var $inspected_result_2 = row.inspected_result_2;
                         if(data == "通过" || data == "折扣通过" || data == "郊区通过" || data == "内部通过")
                         {
